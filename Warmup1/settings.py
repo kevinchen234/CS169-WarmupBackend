a39b12a6dd2fb11802a2b97183188d72b2c34817
Kevin Chen <kevin.chen234@gmail.com>
2013-02-15 17:59:38 -0800

    forgot comma

--- Warmup1/settings.py
+++ Warmup1/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
       #  http://stackoverflow.com/questions/7695962/postgresql-password-authentication-failed-for-user-postgres
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    },
     
     'heroku': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
